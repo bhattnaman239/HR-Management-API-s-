7a16e6a0b574dab0d226534b59339edb688464d2
Naman Bhatt <nb714@snu.edu.in>
2025-03-31 14:56:47 +0530

    Database.py file

--- app/common/constants/database.py
+++ app/common/constants/database.py
@@ -1,3 +1,3 @@
-# SQLALCHEMY_DATABASE_URL = "sqlite:///app/database/database.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///app/database/database.db" #for database
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.sqlite3"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test_db.sqlite3"#for testing
